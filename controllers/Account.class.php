c03e5044820808f5fce7a4c33baaa62165197434
genghonghao <genghonghao@gmail.com>
2013-10-13 16:19:18 +0800

    用户页面

--- controllers/Account.class.php
+++ controllers/Account.class.php
@@ -8,12 +8,19 @@ class Account extends Base{
 
 		$tasks = $task_model->fetchAllTask();
 
+		$tabs = ["task", "article", "fans", "follower", "send_message", "message", "settings"];
+		$tab = g("tab", true, 'task');
+		if (!in_array($tab, $tabs)) {
+			$tab = 'task';
+		}
+
 		$user_name = g("user_name", true, '');
 
 		$user_info = $user_model->getUserByName($user_name);
 		$user_info['extend'] = unserialize($user_info['extend']);
 		$user_info['user_registration_timeline'] = date("Y-m-d H:i:s", $user_info['user_registration_timeline']);
 		
+		$this->tpl->assign('tab',$tab);
 		$this->tpl->assign('tasks',$tasks);
 		$this->tpl->assign('user_info',$user_info);
 		$this->tpl->display('account.html');
