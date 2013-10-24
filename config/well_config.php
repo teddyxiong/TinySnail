6ba6af331cf752d842b4ffc459df54238fe6190e
genghonghao <genghonghao@gmail.com>
2013-10-24 11:04:28 +0800

    显示调整

--- config/well_config.php
+++ config/well_config.php
@@ -2,11 +2,12 @@
 $well_config = [];
 
 // 首页
-$well_config['Index'] = "首页";
+$well_config['Index'] = "你必需非常努力，你才能毫不费力。";
 // 添加任务页
-$well_config['AddTask'] = "任务添加页";
+$well_config['AddTask'] = "没有学到死，就往死里学。";
 // 文章添加页
-$well_config['AddArticle'] = "文章添加页";
+$well_config['AddArticle'] = "Nobody gets to write your destiny but you. 
+除了你自己，没有人能书写你的命运。";
 // 用户详情页
-$well_config['Account'] = "用户中心";
+$well_config['Account'] = "不幸发生时你有三个选择：要么被它左右，要么被它打败，要么就让它把你变得更加强大。";
 ?>
