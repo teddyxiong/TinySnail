ae9998de47e6aceba0422fd7fcf2a6ec20905543
root <root@AY1212121247240a27123.(none)>
2013-10-25 17:09:59 +0800

    del comment

--- snail.sql
+++ snail.sql
@@ -1,27 +1,4 @@
--- MySQL dump 10.13  Distrib 5.5.25a, for Linux (x86_64)
---
--- Host: localhost    Database: snail
--- ------------------------------------------------------
--- Server version	5.5.25a
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `bind_users`
---
-
 DROP TABLE IF EXISTS `bind_users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bind_users` (
   `binduid` bigint(20) NOT NULL AUTO_INCREMENT,
   `source_uid` int(11) NOT NULL DEFAULT '0',
@@ -34,19 +11,8 @@ CREATE TABLE `bind_users` (
   `bind_dateline` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`binduid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `bind_users`
---
-
---
--- Table structure for table `followers`
---
 
 DROP TABLE IF EXISTS `followers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `followers` (
   `fid` bigint(20) NOT NULL AUTO_INCREMENT,
   `uid` bigint(20) NOT NULL,
@@ -60,20 +26,9 @@ CREATE TABLE `followers` (
   KEY `uid` (`uid`),
   KEY `follower_uid` (`follower_uid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `followers`
---
-
-
---
--- Table structure for table `task_article`
---
 
 DROP TABLE IF EXISTS `task_article`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `task_article` (
   `aid` bigint(20) NOT NULL AUTO_INCREMENT,
   `cate_id` int(10) NOT NULL DEFAULT '0',
@@ -92,19 +47,8 @@ CREATE TABLE `task_article` (
   KEY `uid` (`uid`),
   KEY `tid` (`tid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `task_article`
---
-
---
--- Table structure for table `task_attach_info`
---
 
 DROP TABLE IF EXISTS `task_attach_info`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `task_attach_info` (
   `aiid` bigint(20) NOT NULL AUTO_INCREMENT,
   `uid` bigint(20) NOT NULL,
@@ -115,19 +59,8 @@ CREATE TABLE `task_attach_info` (
   PRIMARY KEY (`aiid`),
   KEY `tid` (`tid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `task_attach_info`
---
-
---
--- Table structure for table `task_comments`
---
 
 DROP TABLE IF EXISTS `task_comments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `task_comments` (
   `cid` bigint(20) NOT NULL AUTO_INCREMENT,
   `tid` bigint(20) NOT NULL,
@@ -140,20 +73,8 @@ CREATE TABLE `task_comments` (
   KEY `uid` (`uid`),
   KEY `tid` (`tid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=52 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `task_comments`
---
-
-
---
--- Table structure for table `task_comments_reply`
---
 
 DROP TABLE IF EXISTS `task_comments_reply`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `task_comments_reply` (
   `rid` bigint(20) NOT NULL AUTO_INCREMENT,
   `cid` bigint(20) NOT NULL DEFAULT '0',
@@ -167,19 +88,8 @@ CREATE TABLE `task_comments_reply` (
   PRIMARY KEY (`rid`),
   KEY `uid` (`uid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `task_comments_reply`
---
-
---
--- Table structure for table `task_complete_details`
---
 
 DROP TABLE IF EXISTS `task_complete_details`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `task_complete_details` (
   `did` bigint(20) NOT NULL AUTO_INCREMENT,
   `tid` bigint(20) NOT NULL,
@@ -189,19 +99,8 @@ CREATE TABLE `task_complete_details` (
   PRIMARY KEY (`did`),
   KEY `tid` (`tid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `task_complete_details`
---
-
---
--- Table structure for table `task_votes`
---
 
 DROP TABLE IF EXISTS `task_votes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `task_votes` (
   `vid` bigint(20) NOT NULL AUTO_INCREMENT,
   `tid` bigint(20) NOT NULL,
@@ -215,24 +114,8 @@ CREATE TABLE `task_votes` (
   KEY `uid` (`uid`),
   KEY `tid` (`tid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `task_votes`
---
-
-LOCK TABLES `task_votes` WRITE;
-/*!40000 ALTER TABLE `task_votes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `task_votes` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tasks`
---
 
 DROP TABLE IF EXISTS `tasks`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tasks` (
   `tid` bigint(20) NOT NULL AUTO_INCREMENT,
   `uid` int(10) NOT NULL DEFAULT '0',
@@ -259,19 +142,8 @@ CREATE TABLE `tasks` (
   KEY `status` (`status`),
   KEY `task_cate_id` (`task_cate_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tasks`
---
-
---
--- Table structure for table `user_score_log`
---
 
 DROP TABLE IF EXISTS `user_score_log`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_score_log` (
   `slid` bigint(20) NOT NULL AUTO_INCREMENT,
   `uid` bigint(20) NOT NULL,
@@ -282,20 +154,8 @@ CREATE TABLE `user_score_log` (
   PRIMARY KEY (`slid`),
   KEY `uid` (`uid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_score_log`
---
-
-
---
--- Table structure for table `users`
---
 
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `uid` bigint(20) NOT NULL AUTO_INCREMENT,
   `binduid` int(11) NOT NULL DEFAULT '0',
@@ -318,20 +178,4 @@ CREATE TABLE `users` (
   UNIQUE KEY `user_name` (`user_name`),
   UNIQUE KEY `user_email` (`user_email`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `users`
---
-
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2013-10-25 16:56:09
